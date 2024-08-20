1423ff3b3a02e72151108c6ae0cef12157af1477
Kooreis <kooreis.official@gmail.com>
2024-08-20 12:13:15 -0500

    In the second update, a function named 'CountDistinctSubsequences' is defined, which takes a string as input. The length of the string is stored in 'n'. Two arrays, 'last' and 'dp', are initialized. The 'dp' array is used to store the number of distinct subsequences for all prefixes of the string, and the 'last' array is used to store the last occurrence of all possible characters. The 'dp' array is initialized with 1 at the 0th index, and the 'last' array is initialized with -1 at all indices.

--- solution.cs
+++ solution.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main()
+static int CountDistinctSubsequences(string str)
     {
-        Console.Write("Enter a string: ");
-        string input = Console.ReadLine();
-    }
+        int n = str.Length;
+        int[] last = new int[256];
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        for (int i = 0; i < 256; i++)
+            last[i] = -1;
